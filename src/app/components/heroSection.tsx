52159d8e46d4e33293570ea919a630aedf293276
Vishal Jangid <vishal.jangid_it22@pccoer.in>
2024-07-09 20:23:50 +0530

    Hero section color change 3

--- src/app/components/heroSection.tsx
+++ src/app/components/heroSection.tsx
@@ -6,7 +6,7 @@ import { IoArrowDownCircleOutline } from "react-icons/io5";
 
 function heroSection({title, content}:any) {
   return (
-    <div className="hero min-h-screen border-b-4 sm:top-0 -z-30 bg-grey-">
+    <div className="hero min-h-screen border-b-4 sm:top-0 -z-30 bg-base-200">
   <div className="hero-content text-center">
     <div className="max-w-md relative ">
       <h1 className=" md:mt-0 text-5xl lg:text-8xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-300 font-Bask">{title}</h1>
